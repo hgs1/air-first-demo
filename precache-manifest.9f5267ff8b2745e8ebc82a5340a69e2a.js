afe7b78257732324bb88adf1a57017e6bebb6db3
mayuanchi <mayuanchi1029@gmail.com>
2020-07-08 23:35:06 +0800

    Updates

---
+++ precache-manifest.9f5267ff8b2745e8ebc82a5340a69e2a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271c989d89066c02a13e457516657b30",
+    "url": "/air-first-demo/index.html"
+  },
+  {
+    "revision": "47f05e0f1b7859e4a11c",
+    "url": "/air-first-demo/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "2d42cbd41f0a6c34d40f",
+    "url": "/air-first-demo/static/css/main.fc6a1029.chunk.css"
+  },
+  {
+    "revision": "47f05e0f1b7859e4a11c",
+    "url": "/air-first-demo/static/js/2.1ab93553.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/air-first-demo/static/js/2.1ab93553.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d42cbd41f0a6c34d40f",
+    "url": "/air-first-demo/static/js/main.91f7aefa.chunk.js"
+  },
+  {
+    "revision": "715291db1018a1e2c3bb",
+    "url": "/air-first-demo/static/js/runtime-main.eb76dec2.js"
+  }
+]);
